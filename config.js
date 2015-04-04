631100313f24e7f69cd0ed9c2bf1a972ce1d4e63
Hayden Schiff <oxguy3@gmail.com>
2015-04-04 13:34:14 -0400

    typo fix

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config.web = {};
 
 config.twitch.redirectUri   = process.env.TWITCH_REDIRECT_URI || "http://localhost:3000/auth/"
 config.twitch.clientId      = process.env.TWITCH_CLIENT_ID || "nj4s2qsn6eu6v3xxaia0pgocd97mtz7";
-config.twitch.clientSecret  = process.env.TWITCH_CLIENT_SECRET || "secret"
+config.twitch.clientSecret  = process.env.TWITCH_CLIENT_SECRET || "secret";
 config.twitch.scope         = process.env.TWITCH_SCOPE || "user_read";
 
 config.web.port = process.env.WEB_PORT || 3000;
